fab22c3dd8502a88b39c8dabe4c5aaafabfb3f7e
Nenad Nikolić <nnenad@gmail.com>
2015-03-25 19:19:37 +0100

    settings

--- squaresensor/wsgi.py
+++ squaresensor/wsgi.py
@@ -8,7 +8,11 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
 import os
+from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "squaresensor.settings.local")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+#application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+application = DjangoWhiteNoise(application)
