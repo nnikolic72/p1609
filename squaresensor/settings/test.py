792d2daf32c9ec1a3aa4e16b98521b06b02d64d6
nnikolic72 <nnenad@gmail.com>
2015-04-21 14:30:25 +0200

    unit tests, new friends fix

--- squaresensor/settings/test.py
+++ squaresensor/settings/test.py
@@ -1,4 +1,3 @@
-__author__ = 'tanja'
 from squaresensor.settings.base import *
 import os
 import dj_database_url
@@ -9,53 +8,27 @@ import dj_database_url
 #CACHES = memcacheify()
 
 
-config = ConfigParser()
-settings_path = PROJECT_DIR.child('squaresensor').child("settings")
-settings_path = Path(settings_path, 'settings.ini')
-config.read(settings_path)
-
-SECRET_KEY = config.get('squaresensor', 'APP_SECRET_KEY')
-DATABASE_USER = config.get('database', 'DATABASE_USER')
-DATABASE_PASSWORD = config.get('database', 'DATABASE_PASSWORD')
-DATABASE_HOST = config.get('database', 'DATABASE_HOST')
-DATABASE_PORT = config.get('database', 'DATABASE_PORT')
-DATABASE_ENGINE = config.get('database', 'DATABASE_ENGINE')
-DATABASE_NAME = config.get('database', 'DATABASE_NAME')
-
-
-#INSTAGRAM_API_KEY = config.get('instagram', 'INSTAGRAM_API_KEY')
-
-
-DATABASES = {
-"default": {
-    "ENGINE": "django.db.backends.sqlite3",
-    "NAME": ":memory:",
-    "USER": "",
-    "PASSWORD": "",
-    "HOST": "",
-    "PORT": "",
-    },
-}
-
-
-MEDIA_ROOT = PROJECT_DIR.child("media")
-STATIC_ROOT = PROJECT_DIR.child("static")
-MEDIA_URL = '/media/'
-STATICFILES_DIRS = (
-    PROJECT_DIR.child("assets"),
-)
-
-
+SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
 TEMPLATE_DEBUG = True
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
-INSTALLED_APPS += ('debug_toolbar',)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, '../../static'),
+)
+DATABASES = {
+    'default': dj_database_url.config(default='sqlite:///db.sqlite')
+}
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 MIDDLEWARE_CLASSES += ('django.middleware.clickjacking.XFrameOptionsMiddleware',)
+
+
 
 
